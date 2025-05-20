1e0ea47841e6c4a8bad4a48c38ed95b09b199c0b
arafath <arafathmd432@gmail.com>
2025-05-20 22:07:27 +0600

    fixed hostname 0.0.0.0

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const dev = require('./config');
 const port = dev.app.port;
-const hostName = "127.0.0.1";
+const hostName = "0.0.0.0";
 app.listen(port,hostName,(err)=>{
     if(err) console.log(`Sorry erro:${err}`);
     else console.log(`Your server is runing at:http://${hostName}:${port}`);
